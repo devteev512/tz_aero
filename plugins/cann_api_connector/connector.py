66c8d0a52d3d9417bd0048f62b9df675ea5f0dc0
devteev512 <118401403+devteev512@users.noreply.github.com>
2023-07-12 12:41:53 +0400

    Update connector.py

--- plugins/cann_api_connector/connector.py
+++ plugins/cann_api_connector/connector.py
@@ -29,7 +29,7 @@ class CannApiOperator(BaseOperator):
             (id int PRIMARY KEY, \
              uid text,  \
              strain text, \
-             cannabinoid_abreviation text, \
+             cannabinoid_abbreviation text, \
              cannabinoid text,  \
              terpene text, \
              medical_use text,    \
@@ -56,11 +56,18 @@ class CannApiOperator(BaseOperator):
         conn = self.connect_to_db()
         cursor = conn.cursor()
         self.prepare_db_table(cursor)
-        sql = 'copy cann_data (id, uid, strain, cannabinoid_abreviation, cannabinoid, terpene, \
+        sql = 'copy cann_data (id, uid, strain, cannabinoid_abbreviation, cannabinoid, terpene, \
         medical_use, health_benefit, category, type, buzzword, brand) from stdin '
         with cursor.copy(sql) as copy:
             for record in self.get_data(self.url):
-                copy.write_row(tuple(record.values()))
+                logging.info(record)
+                try:
+                    copy.write_row((record['id'], record['uid'], record['strain'], record['cannabinoid_abbreviation'], record['cannabinoid'], 
+                                record['terpene'], record['medica_use'], record['health_benefit'], record['category'], record['type'],  
+                                record['buzzword'],  record['brand']))
+                except KeyError as e:
+                    logging.warning(f"Got a KeyError with key {e}")
+                    pass
         logging.info("DATA LOADED INTO TABLE CANN_DATA")
         conn.commit()
         conn.close()
